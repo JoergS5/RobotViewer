22cc6af729a8085a717a0dbb1dbd8e481c74b661
JoergS5 <joerg.schnur@outlook.com>
2022-05-11 22:36:47 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 'use strict'
 
-import i18n from '../../i18n'
 import { registerRoute } from '../../routes';
 import { registerPluginContextMenuItem, ContextMenuType } from '../index.js';
-import { registerPluginData, PluginDataType } from '../../store';
 
 
-import Vue from 'vue'
-import App from './App.vue'
+//import Vue from 'vue'
+import RobotViewer from './RobotViewer.vue'
+
+/*
 new Vue({
   render: h=> h(App)
 }).$mount('#app');
+*/
 
 
 
@@ -18,12 +19,12 @@ registerRoute(RobotViewer, {
 	Job: {
 		RobotViewer: {
 			icon: 'mdi-rotate-3d',
-			caption: 'plugins.robotviewer.menuCaption',
+			caption: 'RobotViewer',
 			path: '/Job/RobotViewer'
 		}
 	}
 });
 
-registerPluginContextMenuItem(() => i18n.t('plugins.gcodeViewer.view3D'), '/Job/RobotViewer', 'mdi-rotate-3d', 'view-3d-model', ContextMenuType.JobFileList);
-registerPluginData('RobotViewer', PluginDataType.machineCache, 'robotDHtype', 'RRRRRR');
-registerPluginData('RobotViewer', PluginDataType.machineCache, 'toolColors', ['#00FFFF', '#FF00FF', '#FFFF00', '#000000', '#FFFFFF']);
+registerPluginContextMenuItem(() => 'RobotViewer', '/Job/RobotViewer', 'mdi-rotate-3d', 'view-3d-model', ContextMenuType.JobFileList);
+
+
